c12bb61784011ff73a61ea146e71e1deeb83edd9
AndreyFinfo <f008f@mail.ru>
2023-03-10 17:37:46 +0300

    add input number5

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число:");
+int number5 =Convert.ToInt32(Console.ReadLine());
+while (number5<10000 && number5>99999)
+{
+    Console.Write("Вы должны ввести пятизначное число:");
+    number5 =Convert.ToInt32(Console.ReadLine());
+}
+
