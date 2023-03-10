3f67f8d1cf969c395f3f59f6f1e010446ec837cf
AndreyF <f008f@mai;.ru>
2023-03-10 20:42:42 +0300

    Tusk19 modifed

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -7,22 +7,30 @@
 
 Console.Write("Введите пятизначное число: ");
 int number5 =Convert.ToInt32(Console.ReadLine());
+//проверка, чтобы число состояло из 5 знаков
 while (number5<10000 || number5>99999)
 {
     Console.WriteLine("Вы должны ввести пятизначное число! ");
     number5 =Convert.ToInt32(Console.ReadLine());
 }
 
+// перевод int в String
 String string5 = string.Empty;
 string5 = number5.ToString();
 
-for (int i = 0; i<=(string5.Length/2); i++)
+// функция(метод) проверяет на палидром
+void CheckPalindrom(String str)
 {
-    if (string5[i] != string5[string5.Length-1-i])
+        for (int i = 0; i<=(str.Length/2); i++)
     {
-        Console.Write("Введённое число не является палиндромом.");
-        break;
+        if (str[i] != str[str.Length-1-i])
+        {
+            Console.Write("Введённое число не является палиндромом.");
+            break;
+        }
+        if (i==str.Length/2) Console.WriteLine("Введённое число является палиндромом!");
     }
-    if (i==string5.Length/2) Console.WriteLine("Введённое число является палиндромом!");
 }
 
+// вызов функции(метода) с подачей нашего числа в качестве аргумента в виде строки
+CheckPalindrom(string5);
